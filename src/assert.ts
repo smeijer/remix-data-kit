f2629b30e33d750226645cc3699f8efa939b7ce2
Stephan Meijer <stephan.meijer@gmail.com>
2024-05-09 16:06:16 +0200

    feat: add isAssertionError util

--- src/assert.ts
+++ src/assert.ts
@@ -1,15 +1,51 @@
 import { json } from '@remix-run/server-runtime';
 import { Static, TSchema } from '@sinclair/typebox';
+import { ValueError, ValueErrorType } from '@sinclair/typebox/errors';
 import { assert, AssertionError } from 'typebox-assert';
+
+type SerializedAssertionError = {
+	ok: false;
+	message: string;
+	errors: ValueError[];
+};
 
 export function assertType<T extends TSchema>(schema: T, value: unknown, message?: string): asserts value is Static<T> {
 	try {
 		assert(schema, value, message);
 	} catch (e) {
 		if (e instanceof AssertionError) {
-			throw json({ ok: false, message: e.message, errors: e.errors });
+			throw json<SerializedAssertionError>({ ok: false, message: e.message, errors: e.errors });
 		}
 
 		throw e;
 	}
+}
+
+const knownErrorValues = new Set<number>(
+	Object.values(ValueErrorType).flatMap((x) => (typeof x === 'number' ? [x] : [])),
+);
+
+function isValueError(value: unknown): value is ValueError {
+	if (value == null || typeof value !== 'object') return false;
+	if (!('type' in value) || typeof value.type !== 'number' || !knownErrorValues.has(value.type)) return false;
+	if (!Object.values(ValueErrorType).includes(value.type)) return false;
+	if (!('message' in value) || typeof value.message !== 'string') return false;
+	if (!('path' in value) || typeof value.path !== 'string') return false;
+	if (!('value' in value)) return false;
+	if (!('schema' in value) || typeof value.schema !== 'object') return false;
+	return true;
+}
+
+/**
+ * Check if provided `value` is a valid AssertionError. This util is to be used in
+ * components to see if a fetcher caught an AssertionError, thrown by the action
+ * @param value
+ */
+export function isAssertionError(value: unknown): value is SerializedAssertionError {
+	if (value == null || typeof value !== 'object') return false;
+	if (!('ok' in value) || value.ok !== true) return false;
+	if (!('message' in value) || typeof value.message !== 'string') return false;
+	if (!('errors' in value) || !Array.isArray(value.errors)) return false;
+	if (!value.errors.every((x) => isValueError(x))) return false;
+	return true;
 }
