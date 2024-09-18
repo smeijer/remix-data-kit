70c50b7348a6285d8b3ce8f2e4c9d06f0daa38a9
Stephan Meijer <stephan.meijer@gmail.com>
2024-09-18 18:02:32 +0200

    chore: update publish script

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,4 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access */
+import { readFileSync } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+
 import { execSync } from 'child_process';
+import fs from 'fs';
+import path from 'path';
 
 const arg = process.argv.slice(2);
 const [releaseType] = arg;
@@ -10,4 +16,10 @@ if (!releaseType || !['major', 'minor', 'patch'].includes(releaseType)) {
 
 execSync(`npm version ${releaseType}`, { stdio: 'inherit' });
 execSync(`npm run build`, { stdio: 'inherit' });
-execSync(`npm publish ./dist`, { stdio: 'inherit' });
+execSync(`npm publish`, { stdio: 'inherit' });
+execSync(`git add package.json package-lock.json`);
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const pkgJson = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'package.json'), 'utf-8'));
+
+execSync(`git commit -m 'chore: bump version to ${pkgJson.version}`);
